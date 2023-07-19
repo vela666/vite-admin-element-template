2c4751014c72617275c8cd05f936d42b14ccdea1
wyl <1052063507@qq.com>
2023-07-19 10:51:45 +0800

    chore: 更新脚本

--- pushAll.sh
+++ pushAll.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # 推送多个
+# 需先关联
+# git remote add  gitee  git@gitee.com:gongxi199937/vite-admin-element-template.git
+# git remote add  github  git@github.com:vela666/vite-admin-element-template.git
+# git pull 仓库名 master
 git push -u gitee master
 git push -u github master
