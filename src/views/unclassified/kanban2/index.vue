a2ca9c32290d6c729fce6957e4c83e19631368a6
wyl <1052063507@qq.com>
2023-07-21 16:19:21 +0800

    perf: mock依赖 降级

--- src/views/unclassified/kanban2/index.vue
+++ src/views/unclassified/kanban2/index.vue
@@ -5,28 +5,16 @@
     <el-button @click="save">应用</el-button>
     <br />
     <GridLayout dragHandle=".handler" ref="gridLayoutRef" v-model="items" />
-
     <LayoutManage :data="items" ref="layoutManageRef" @save="kanBanSave" />
   </main>
 </template>
 
 <script setup>
-import {
-  computed,
-  watch,
-  reactive,
-  ref,
-  nextTick,
-  onMounted,
-  shallowRef,
-  onActivated,
-} from 'vue'
+import { ref, nextTick, onMounted, shallowRef, onActivated } from 'vue'
 import 'gridstack/dist/gridstack.min.css'
-import { GridStack } from 'gridstack'
 import GridLayout from './components/GridLayout.vue'
 import LayoutManage from './components/LayoutManage.vue'
 import { differenceBy } from 'lodash-es'
-
 const layoutManageRef = shallowRef(null)
 // const items = ref([])
 const items = ref(
@@ -39,22 +27,15 @@ const items = ref(
       y: 0,
       data: [1],
     },
-    {
+    /* {
       id: '2',
       w: 3,
       h: 2,
       x: 9,
       y: 0,
       data: [2],
-    },
-    {
-      id: '3',
-      w: 6,
-      h: 4,
-      x: 6,
-      y: 2,
-      data: [3],
-    },
+    },*/
+
     {
       id: '4',
       w: 12,
@@ -62,6 +43,12 @@ const items = ref(
       x: 0,
       y: 6,
       data: [4],
+    },
+    {
+      id: '3',
+      w: 6,
+      h: 4,
+      data: [3],
     },
   ].map((item) => {
     return {
@@ -97,22 +84,22 @@ const items = ref(
 const gridLayoutRef = ref(null)
 
 const addNewKanBan = () => {
-  const id = (+items.value.at(-1)?.id || 0) + 1
+  const id = self.crypto.randomUUID()
   const node = {
     // x: 0, // 初始位置 x
     // y: 0, // 初始位置 y
     // autoPosition: true,
-    w: id % 2 ? 6 : 12,
+    w: 6,
     // w: 6,
     // h: id % 2 ? 4 : 2,
     // h: 4,
-    id: id + '',
+    id,
     mark: `grid-item-${id}`,
     // x: 0, // 初始位置 x
     // y: 0, // 初始位置 y
     // autoPosition: true,
     // w: id % 2 ? 6 : 12,
-    minH: id % 2 ? 4 : 2,
+    minH: 4,
   }
   items.value.push(node) // 将新网格项添加到数据数组中
   gridLayoutRef.value.makeLayout(node.mark)
@@ -123,7 +110,12 @@ const managementKanBan = () => {
 }
 
 const kanBanSave = (data) => {
+  // 查找 差集 其中一边不存在的数据
+  const differenceVals = differenceBy(data, items.value, 'id')
   items.value = data
+  differenceVals.forEach((item) => {
+    gridLayoutRef.value.makeLayout(item.mark)
+  })
   gridLayoutRef.value.reloadLayout(data)
 }
 
